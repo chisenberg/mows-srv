965a7336c70ddefce623a183ecc59e86da72cc09
daysoft_christian <christianisenberg@gmail.com>
2018-08-27 19:05:41 -0300

    Changes that i dont remember

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const { spawn } = require('child_process');
+const express = require('express');
+const app = express();
+
 const Database  = require('./lib/database.js');
-const db = new Database('dados.db');
+const db = new Database(__dirname +'/dados.db');
+
+const BATTERY_CONSTANT = (3.3/256) * 1.422;
 
 const pulse433 = spawn('./bin/pulse433', []);
 
-const BATTERY_CONSTANT = (3.3/256) * 1.422;
+
+app.get('/', function (req, res) {
+	db.last().then((rows) => {
+		res.send(rows[0]);
+	});
+});
+
+const server = app.listen(8080, function () {
+	let port = server.address().port;
+	console.warn('listening at ', port);
+});
 
 pulse433.stdout.on('data', (data) => {
 	let dataArray = data.toString().split(';');
@@ -14,21 +29,19 @@ pulse433.stdout.on('data', (data) => {
 	var humid = parseFloat(((parseInt(dataArray[4]) * 256 + parseInt(dataArray[5])) / 10).toFixed(2));
 	var batt = parseFloat((parseInt(dataArray[1]) * BATTERY_CONSTANT).toFixed(2));
 
-	let log = db.log({
-		temp: temp,
-		humid: humid,
-		batt: batt
-	});
-	
-	log.then((rows) => {
-		console.warn('then :) ', rows);
-	}, (err) => {
-		console.error('catch :( ', JSON.stringify(err));
+	db.log({
+		temperature: temp,
+		humidity: humid
+	}).catch((err) => {
+		// console.error('CATCHAU', err);
 	});
 
-	console.warn('#', nMsg, batt, temp, humid);
+	// db.last({
+	// 	temperature: temp,
+	// 	humidity: humid
+	// }).then((rows) => {
+	// 	console.warn(rows);
+	// });
 });
 
-process.on('unhandledRejection', error => {
-	console.error('unhandledRejection', error.message);
-});
+
